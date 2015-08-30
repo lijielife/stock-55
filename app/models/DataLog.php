d4a77e6abac06cc6ae354c654e4b3affce44c7d5
momo <momo@momo-pc>
2015-08-30 22:04:29 +0700

    H logs active V2

--- app/models/DataLog.php
+++ app/models/DataLog.php
@@ -6,5 +6,6 @@ class DataLog extends Model{
         protected $fillable = array('ID', 'SIDE_ID', 'SYMBOL_ID', 'VOLUME', 'PRICE', 'AMOUNT', 'VAT', 'at_pay'
             , 'NET_AMOUNT', 'DATE', 'BROKER_ID', 'CREATED_AT', 'CREATED_BY'
             , 'UPDATED_AT', 'UPDATED_BY', 'USER_ID');
-    
+ 
+        
 }
