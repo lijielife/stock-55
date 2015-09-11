b7fb46fabc1eee20c73e642bc040bf970e0ba008
miccgood <micverygood@hotmail.com>
2015-09-11 17:29:37 +0700

    W LogsProfile

---
+++ sql/query_logs_profile.sql
@@ -0,0 +1,18 @@
+SELECT DISTINCT
+            da.*, GROUP_CONCAT(ma.MAP_DESC SEPARATOR ',')
+            
+        FROM super_stock_db.DATA_LOG da
+        LEFT JOIN super_stock_db.LOG_MAP ma on (da.ID = ma.MAP_SRC)
+        LEFT JOIN super_stock_db.DATA_LOG dad on (dad.ID = ma.MAP_DESC)
+        LEFT JOIN super_stock_db.MAS_SIDE msd on (msd.id = dad.SIDE_ID)
+        LEFT JOIN super_stock_db.MAS_SYMBOL msy on (msy.id = da.SYMBOL_ID)
+        LEFT JOIN super_stock_db.MAS_SYMBOL msyd on (msyd.id = dad.SYMBOL_ID)
+        LEFT JOIN super_stock_db.MAS_BROKER mbk ON (da.BROKER_ID = mbk.ID)
+        LEFT JOIN super_stock_db.MAS_BROKER mbkd ON (dad.BROKER_ID = mbkd.ID)
+        WHERE da.USER_ID = 1 
+            
+            AND ('BLAND' IS null OR msy.SYMBOL = 'BLAND')
+            AND (1 IS null OR mbk.ID = 1)
+            
+        GROUP BY da.ID
+        ORDER BY da.`DATE`
