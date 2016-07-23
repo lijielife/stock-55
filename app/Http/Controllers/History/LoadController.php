92cbe9b84957fd8009ed8180a536a223e786c941
momo <momo@momo-pc>
2016-07-23 19:03:40 +0700

    update

--- app/Http/Controllers/History/LoadController.php
+++ app/Http/Controllers/History/LoadController.php
@@ -24,23 +24,22 @@ class LoadController extends GetController {
     public function loadData() {
         set_time_limit(0);
 
-        
-        $symbols = (Request::input('symbols') === null ? null : 
-                explode(",", Request::input('symbols')));
-        
+
+        $symbols = (Request::input('symbols') === null ? null : explode(",", Request::input('symbols')));
+
         $isLoadAll = (Request::input('isLoadAll') === null ? false : true);
         $this->log->info(" isLoadAll : " . '' . ($isLoadAll ? 'true' : 'false') );
-        
+
         if($isLoadAll){
             $this->resetData();
         } else {
             $this->resetDataInPort($symbols);
         }
-        
+
 //        $respone = array();
 
         $masSymbols = $this->getSymbolIsUse();
-        
+
         $statusArr = array();
         $sessionId = Session::getId();
         foreach ($masSymbols as $masSymbol) {
@@ -52,9 +51,9 @@ class LoadController extends GetController {
             $status->setCreate_time(date('H:i:s'));
             $statusArr[$masSymbol] = (array) $status;
         }
-        
+
         DB::table('LOAD_STATUS')->where('SESSION_ID', $sessionId)->delete();
-        
+
         DB::table("LOAD_STATUS")->insert($statusArr);
 //        \Illuminate\Contracts\Queue\ShouldQueue::
 //        $threadedMethod = new LoadControllerThread($this);
@@ -62,49 +61,49 @@ class LoadController extends GetController {
 
 //        load normal loop
         $this->loadDataLoop($masSymbols, $statusArr);
-        
+
 //        load error loop
         $loadErrorSymbols = $this->getLoadStatusError();
         $this->loadDataLoop($loadErrorSymbols, $statusArr);
-        
+
 
 //        reset data
         $this->resetDataInPort();
-        
+
 //        if(empty($respone)){
             $respone = parent::getCount($sessionId);
 //        }
         return json_encode($respone);
     }
 
-    
-    
+
+
     public function executeByThread($masSymbols, $statusArr) {
-        
+
 //        $this->loadDataLoop($masSymbols, $statusArr);
-//        
+//
 //        $loadErrorSymbols = $this->getLoadStatusError();
 //        $this->loadDataLoop($loadErrorSymbols, $statusArr);
-        
-        
+
+
     }
-    
+
     public function loadDataLoop($masSymbols, $statusArr) {
-        
+
         foreach ($masSymbols as $masSymbol) {
             if (strpos($masSymbol, "&") !== false) {
                 continue;
             }
 
             $this->log->info($masSymbol);
-            
+
             $error_desc = null;
             $status_desc = 'success';
             try {
                 $data = $this->process($masSymbol);
 //                array_push($respone, array("symbolName" => $masSymbol
 //                    , "count" => $data->count));
-                
+
             } catch (\Exception $e) {
                 $this->log->info(" Error LoadData Symbol : " . $masSymbol );
                 $error_desc = $e->getMessage();
@@ -114,14 +113,14 @@ class LoadController extends GetController {
                 $status = $statusArr[$masSymbol];
                 DB::table('LOAD_STATUS')
                     ->where($status)
-                    ->update(['STATUS_DESC' => $status_desc, 
+                    ->update(['STATUS_DESC' => $status_desc,
                         'ERROR_DESC' => $error_desc]);
 
                 usleep(1000);
             }
         }
     }
-    
+
     public function getStatus($origin = 'investor') {
         $sessionId = Session::getId();
         return json_encode(parent::getStatus($sessionId));
